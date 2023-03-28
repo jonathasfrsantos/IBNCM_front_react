03087c2e545ff6da49a26290ad1d191f6a0fa5a5
jonathasfrsantos <jonathasfrsantos@gmail.com>
2023-03-27 23:19:55 -0300

    clearform preserve date

--- src/utils/ClearForm.js
+++ src/utils/ClearForm.js
@@ -1,14 +1,14 @@
-
 function ClearForm(setTransaction) {
-    setTransaction({
-      data: "",
-      valor: 0.0,
-      entrada: 0.0,
-      saida: 0.0,
-      historico: "",
-      finalidade: "",
-      bancoCaixa: "",
-    });
-  }
+  setTransaction((prevTransaction) => ({
+    ...prevTransaction,
+    valor: 0.0,
+    entrada: 0.0,
+    saida: 0.0,
+    historico: "",
+    finalidade: "",
+    bancoCaixa: "",
+  }));
+}
+
 export default ClearForm;  
 
